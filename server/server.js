36e1188da26ea786a54e0672555e369032e81cb6
rythm <jgon@mit.edu>
2024-01-25 16:46:46 -0600

    setup ejs w/ express and expose public directory, host index page linking css

--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.listen(port);
 console.log(`Server started at http://localhost:${port}`);
 
 const clientPath = path.resolve(__dirname, '..', 'client');
 
+app.use(express.static(path.join(clientPath, 'public')));
+
+app.set('views', path.join(clientPath, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.sendFile(path.join(clientPath, 'index.html'));
+  res.render('index');
 });
