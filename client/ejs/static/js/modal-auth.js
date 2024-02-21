c282e24b05584c67d904337d499f40efcc646168
rythm <jgon@mit.edu>
2024-02-20 19:30:58 -0600

    add modal popup functionality

---
+++ client/ejs/static/js/modal-auth.js
@@ -0,0 +1,23 @@
+var loginButtons = document.getElementsByClassName('login-container__button');
+var closeButtons = document.getElementsByClassName('auth-modal__close');
+
+Array.from(loginButtons).forEach(button => {
+  button.onclick = () => {
+    modalId = button.id.split('-popup')[0];
+    modal = document.getElementById(modalId);
+    modal.classList.add('auth-modal--show');
+  };
+});
+
+Array.from(closeButtons).forEach(button => {
+  button.onclick = () => {
+    modal = button.closest('.auth-modal');
+    modal.classList.remove('auth-modal--show');
+  };
+});
+
+window.onclick = e => {
+  if (e.target.classList.contains('auth-modal')){
+    e.target.classList.remove('auth-modal--show');
+  }
+};
