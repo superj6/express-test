490efce2c9db87e36c5acf2c7dbeedf2eb616dfa
rythm <jgon@mit.edu>
2024-02-22 15:41:59 -0600

    modal-auth add logout functionality

--- server/apis/api.js
+++ server/apis/api.js
@@ -9,4 +9,11 @@ router.post('/ejs/modal-auth/login', passport.authenticate('local', {
   failureRedirect: '/ejs/modal-auth/fail'
 }));
 
+router.post('/ejs/modal-auth/logout', function(req, res, next){
+  req.logout(function(err){
+    if(err){ return next(err);}
+    res.redirect('/ejs/modal-auth');
+  });
+});
+
 module.exports = router;
